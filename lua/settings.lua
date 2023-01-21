72a30a6e1eb21e8fb88a5bd410f99f15c235fb9e
Migue LD <ledesmaduarte@duck.com>
2023-01-21 15:35:03 -0600

    [lua] settings-spell para archivos markdown

--- lua/settings.lua
+++ lua/settings.lua
@@ -13,25 +13,36 @@ g.mapleader = " "
 --UI
 -- Configuraciones de la interface.
 
-opt.syntax = on  		-- sintaxis
-opt.number = true 		-- numeros de linea
+opt.syntax = on  				-- sintaxis
+opt.number = true 			-- numeros de linea
 opt.relativenumber = true	-- numeros de linea relativos
-opt.mouse = a 			-- Uso del mouse
+opt.mouse = a 					-- Uso del mouse
 opt.clipboard = unnamed		-- no me acuerdo
-opt.showcmd = true		-- no me acuerdo
-opt.ruler = true		-- no me acuerdo
+opt.showcmd = true			-- no me acuerdo
+opt.ruler = true				-- no me acuerdo
 opt.encoding = 'UTF-8'		-- codificación
-opt.showmatch = true		-- no me acuerdo
-opt.sw = 2			-- no me acuerdo
-opt.laststatus= 2		-- no me acuerdo
-opt.backspace= '2'		-- no me acuerdo
-opt.guioptions = T		-- no me acuerdo
-opt.guioptions = L		-- no me acuerdo
-opt.numberwidth =1		-- no me acuerdo
+opt.showmatch = true			-- no me acuerdo
+opt.sw = 2						-- no me acuerdo
+opt.laststatus= 2				-- no me acuerdo
+opt.backspace= '2'			-- no me acuerdo
+opt.tabstop = 3 				-- número de caracteres de tab
+opt.shiftwidth = 3			-- borrar los caracteres de tab
+opt.guioptions = T			-- no me acuerdo
+opt.guioptions = L			-- no me acuerdo
+opt.numberwidth =1			-- no me acuerdo
 
 opt.ignorecase = true 		--Las busquedas ingoran el casesesitive
 opt.smartcase= true  		-- a menos que contengan una letra mayuscula.  
 
--- Config cursores horizontal-vertical
-opt.cursorline = true
-opt.cursorcolumn = true
+opt.cursorline = true		-- mostrar cursor horizontal
+opt.cursorcolumn = true		-- mostrar cursor vertical
+
+-- configuración de entornos
+-- por ejemplo para markdown
+exec ([[
+	augroup markdownspell
+		autocmd!
+		autocmd FileType markdown setlocal spell spelllang=es
+		autocmd BufRead, BufNewFile *.md setlocal spell spelllang=es
+	augroup END
+]], false)
